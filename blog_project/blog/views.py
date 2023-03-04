66f339c4e730c254c88b77a192503c286588a1b1
Javohir-dev <97449821+Javohir-dev@users.noreply.github.com>
2023-03-04 12:38:40 +0300

    This project was very short. Now it's over.

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from .models import Blog
 
 
-def bloglistview(request):
-    blogs = Blog.objects.all()
+# def bloglistview(request):
+#     blogs = Blog.objects.all()
+#
+#     context = {
+#         "blogs": blogs,
+#     }
+#     return render(request, "home.html", context=context)
+#
+#
+# def blogdetailview(request, id):
+#     blog = get_object_or_404(Blog, id=id)
+#     context = {
+#         "blog": blog
+#     }
+#     return render(request, "blog_detail.html", context=context)
+class BlogListView(ListView):
+    model = Blog
+    template_name = "home.html"
+    context_object_name = "blogs"
 
-    context = {
-        "blogs": blogs,
-    }
-    return render(request, "home.html", context=context)
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = "blog_detail.html"
+    # context_object_name = "blog"
